8b7d5169976e354157384db3b1ae98fbd6fe45b5
JaeDeokHan <dgd03023@cu.ac.kr>
2019-04-09 14:48:43 +0900

    jumping

--- jump.R
+++ jump.R
@@ -2,17 +2,10 @@ install.packages("dplyr")
 library(dplyr)
 install.packages("ggplot2")
 library(ggplot2)
-displ1 <- mpg %>% filter(displ<=4)
-displ1
-displ2 <- mpg %>% filter(displ>=5)
-displ2
-mean(displ1$hwy)
-mean(displ2$hwy)
+test <- mpg %>% select(class, cty)
+head(test)
 
-mpg_audi <- mpg %>% filter(manufacturer == "audi")
-mpg_toyota <- mpg %>% filter(manufacturer == "toyota")
-mean(mpg_audi$cty)
-mean(mpg_toyota$cty)
-
-mpg_new <- mpg %>% filter(manufacturer %in% c("chevolet","ford","honda"))
-mean(mpg_new$hwy)
+df_suv <- df %>% filter(class == "suv")
+df_compact <- df %>% filter(class == "compact")
+mean(df_suv$cty)
+mean(df_compact$cty)
